975bf2a3b80c1f0bdb60de7e3855251f693aae23
Sohee-Kim-ln <livre6491@gmail.com>
2023-07-31 17:31:50 +0900

    [FE] Fix: index.jsx에서 사라진 리덕스용 Provider 재추가

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -9,9 +9,11 @@ import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
