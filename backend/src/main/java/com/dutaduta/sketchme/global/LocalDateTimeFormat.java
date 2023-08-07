8de400dc45f371f5036b76e8d82c14cff1232eef
KBS <qudtjs3636@gmail.com>
2023-08-07 17:02:22 +0900

    [BE] hotfix: converter 문제 수정

--- backend/src/main/java/com/dutaduta/sketchme/global/LocalDateTimeFormat.java
+++ backend/src/main/java/com/dutaduta/sketchme/global/LocalDateTimeFormat.java
@@ -3,5 +3,5 @@ package com.dutaduta.sketchme.global;
 
 public class LocalDateTimeFormat {
 
-    public static final String DEFAULT = "yyyy-MM-dd'T'HH:mm:ss.SS";
+    public static final String DEFAULT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS";
 }
