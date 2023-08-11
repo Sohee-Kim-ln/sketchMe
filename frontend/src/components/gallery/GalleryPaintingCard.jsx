0153dadfdb98e39f35dac4ba31e2aae7b1d4ff4c
박지원 <cute060500@naver.com>
2023-08-11 13:21:06 +0900

    [FE] Feat: 카테고리에 그림 기능 추가(#64)

--- frontend/src/components/gallery/GalleryPaintingCard.jsx
+++ frontend/src/components/gallery/GalleryPaintingCard.jsx
@@ -1,27 +1,51 @@
-import React, { useState } from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useState, useRef } from 'react';
+import Swal from 'sweetalert2';
 import BaseIconBtnGrey from '../common/BaseIconBtnGrey';
 import BaseTag from '../common/BaseTag';
-import { ReactComponent as CancelIcon } from '../../assets/icons/Cancel.svg';
+import GalleryTag from './GalleryTag';
+import API from '../../utils/api';
 
 function GalleryPaintingCard() {
-  const [isEditing, setIsEditing] = useState(false);
-  const handleEditClick = () => {
-    setIsEditing(!isEditing);
-  };
+  const categoryId = 3;
   const initialData = {
     title: '인물 사진 그려드려용',
     intro: '프로필 이미지 혹은 명함, 액자 제작, 애인이나 부모님께 선물용으로 추억을 만들어 드립니다.',
-    tag: ['인물', '재밌는', '사실적', '반려동물'],
     price: '3000',
+    images: [
+      'https://cdna.artstation.com/p/assets/images/images/023/685/508/large/bbo-art-.jpg?1580033578',
+      'https://mblogthumb-phinf.pstatic.net/MjAxNzA4MjhfNDQg/MDAxNTAzODQ2NDEzMDg2.RJv8W8wIt-hLBGKE4S9CzfPp99tScZOXrKr6QdXDQMkg.wL_yxa2DxAu7RIwIEo29f7AidwZLWWaTrj6n7aLfhr0g.JPEG.beesangu/doggabi.jpg?type=w2',
+      'https://image.newsis.com/2014/09/24/NISI20140924_0010158036_web.jpg'],
+    tags: [{ index: 7, label: '따뜻한' },
+      { index: 8, label: '귀여운' },
+      { index: 9, label: '웃긴' }],
   };
 
   // 원본 데이터와 수정 중인 데이터를 상태로 관리
   const [originalData, setOriginalData] = useState(initialData);
   const [currentData, setCurrentData] = useState(initialData);
+  const imgInputRef = useRef(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleTagChange = (newTags) => {
+    setCurrentData({
+      ...currentData,
+      tags: newTags,
+    });
+  };
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
 
   // input 값이 변경되면 수정 중인 데이터를 업데이트
   const handleChange = (event) => {
     const { name, value } = event.target;
+    if (name === 'title' && value.length > 15) {
+      return;
+    }
+    if (name === 'intro' && value.length > 50) {
+      return;
+    }
     setCurrentData({
       ...currentData,
       [name]: value,
@@ -34,16 +58,99 @@ function GalleryPaintingCard() {
     handleEditClick();
   };
 
+  const editCategory = async () => {
+    Swal.fire({
+      icon: 'warning',
+      title: '카테고리를 수정 하시겠습니까? ',
+      showCancelButton: true,
+      confirmButtonText: '수정',
+      cancelButtonText: '취소',
+    }).then(async (res) => {
+      if (res.isConfirmed) {
+        try {
+          const url = '/api/category';
+          const body = {
+            categoryID: categoryId,
+            name: currentData.title,
+            description: currentData.intro,
+            approximatePrice: currentData.price,
+            hashtags: currentData.tags.map((tag) => tag.index),
+          };
+          const response = await API.put(url, body);
+          console.log(response.data);
+          return response.data;
+        } catch (error) {
+          console.log('Error data:', error.response);
+          throw error;
+        }
+      } else {
+        return null;
+      }
+    });
+  };
+
   const handleComplete = () => {
+    editCategory();
     setOriginalData(currentData);
     handleEditClick();
   };
 
-  const handleRemoveTag = (tag) => {
-    setCurrentData({
-      ...currentData,
-      tag: currentData.tag.filter((item) => item !== tag),
+  const deleteCategory = async () => {
+    Swal.fire({
+      icon: 'warning',
+      title: '카테고리를 삭제 하시겠습니까? ',
+      showCancelButton: true,
+      confirmButtonText: '삭제',
+      cancelButtonText: '취소',
+    }).then(async (res) => {
+      if (res.isConfirmed) {
+        try {
+          const url = '/api/category';
+          const body = {
+            categoryID: categoryId,
+          };
+          const response = await API.delete(url, { data: body });
+          console.log(response.data);
+          return response.data;
+        } catch (error) {
+          console.log('Error data:', error.response);
+          throw error;
+        }
+      } else {
+        return null;
+      }
     });
+  };
+
+  // 파일 선택 버튼을 클릭하면 input 태그를 클릭합니다.
+  const handleImgBtnClick = () => {
+    imgInputRef.current.click();
+  };
+
+  // 이미지 업로드 처리 함수
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      // 파일을 읽어서 이미지 URL을 상태에 업데이트합니다.
+      setCurrentData({
+        ...currentData,
+        images: [...currentData.images, reader.result],
+      });
+    };
+
+    if (file) {
+      // 파일이 존재하면 파일을 읽어옵니다.
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDeleteImage = (index) => {
+    setCurrentData((prevData) => ({
+      ...prevData,
+      images: prevData.images.filter((_, i) => i !== index),
+    }));
   };
 
   return (
@@ -64,21 +171,16 @@ function GalleryPaintingCard() {
       </div>
       <div className="flex mt-1 justify-between">
         <span className="flex ">
-          {currentData.tag.map((item) => (
-            <span key={item} className="mr-2 flex">
-              <span><BaseTag message={item} /></span>
-              <span>
-                {isEditing && (
-                  <CancelIcon className="cursor-pointer pt-1 w-4 h-4" onClick={() => handleRemoveTag(item)} />
-                )}
-              </span>
+          {currentData.tags.map((item) => (
+            <span key={item.index} className="mr-2 flex mt-1">
+              <span><BaseTag message={item.label} /></span>
             </span>
           ))}
         </span>
         <span className="w-fit flex">
           {isEditing ? (
             <span className="flex w-28">
-              <input name="price" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" value={currentData.price} type="text" onChange={handleChange} />
+              <input name="price" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" value={currentData.price} type="number" onChange={handleChange} />
               <span className="min-w-fit mt-2">원 ~</span>
             </span>
           )
@@ -88,6 +190,33 @@ function GalleryPaintingCard() {
                 원~
               </div>
             )}
+        </span>
+      </div>
+      <div className="image-list flex">
+        <span className="flex overflow-x-auto">
+          {currentData.images && currentData.images.map((image, index) => (
+            <div key={index} className="flex-shrink-0 m-2">
+              <img src={image} className="w-32 h-32 object-cover rounded" alt={`${image}`} />
+              {isEditing
+                && (
+                  <button type="button" onClick={() => handleDeleteImage(index)}>X</button>
+                )}
+            </div>
+          ))}
+        </span>
+        <span className="flex items-center">
+          {isEditing
+            && (
+              <BaseIconBtnGrey icon="pencil" message="그림 추가" onClick={handleImgBtnClick} />
+            )}
+          <input
+            type="file"
+            id="imageUpload"
+            accept="image/*"
+            className="hidden"
+            ref={imgInputRef}
+            onChange={handleImageUpload}
+          />
         </span>
       </div>
       <div className="absolute top-10 right-4 hidden md:block">
@@ -104,9 +233,10 @@ function GalleryPaintingCard() {
               <BaseIconBtnGrey onClick={handleEditClick} icon="pencil" message="편집" />
             )}
           </span>
-          <span><BaseIconBtnGrey icon="trash" message="카테고리삭제" /></span>
+          <span><BaseIconBtnGrey icon="trash" message="카테고리삭제" onClick={deleteCategory} /></span>
         </div>
       </div>
+      {isEditing && <GalleryTag tags={currentData.tags} onTagChange={handleTagChange} />}
     </div>
   );
 }
--- frontend/src/components/gallery/GalleryProfileCard.jsx
+++ frontend/src/components/gallery/GalleryProfileCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Swal from 'sweetalert2';
@@ -8,6 +8,11 @@ import BaseIconBtnWhite from '../common/BaseIconBtnWhite';
 import BaseIconBtnGrey from '../common/BaseIconBtnGrey';
 import { ReactComponent as StarIcon } from '../../assets/icons/Star.svg';
 import API from '../../utils/api';
+import GalleryTag from './GalleryTag';
+import BaseTag from '../common/BaseTag';
+import {
+  addSelectedButton,
+} from '../../reducers/SearchSlice';
 
 function GalleryProfileCard() {
   const navigate = useNavigate();
@@ -17,6 +22,12 @@ function GalleryProfileCard() {
   const [isEditing, setIsEditing] = useState(false);
   // input 태그(사진 업로드) 의 참조
   const imgInputRef = useRef(null);
+  const [tags, setTags] = useState([{ index: 7, label: '따뜻한' },
+    { index: 8, label: '귀여운' },
+    { index: 9, label: '웃긴' }]);
+  const handleTagChange = (newTags) => {
+    setTags(newTags);
+  };
   const handleEditClick = () => {
     setIsEditing(!isEditing);
   };
@@ -26,7 +37,6 @@ function GalleryProfileCard() {
     like: '45',
     reviews: '67',
     price: '3000',
-    tag: ['인물', '재밌는', '사실적', '반려동물'],
     profileImg: 'https://source.unsplash.com/vpOeXr5wmR4/600x600',
   };
   // 원본 데이터와 수정 중인 데이터를 상태로 관리
@@ -80,6 +90,34 @@ function GalleryProfileCard() {
     });
   };
 
+  // const editProfile = () => {
+  //   Swal.fire({
+  //     icon: 'warning',
+  //     title: '프로필을 수정 하시겠습니까? ',
+  //     showCancelButton: true,
+  //     confirmButtonText: '수정',
+  //     cancelButtonText: '취소',
+  //   }).then(async (res) => {
+  //     if (res.isConfirmed) {
+  //       try {
+  //         const url = '/api/category';
+  //         const body = {
+  //           categoryID: categoryId,
+  //         };
+  //         const response = await API.delete(url, { data: body });
+  //         console.log(response.data);
+  //         return response.data;
+  //       } catch (error) {
+  //         console.log('Server responded with status:', error.response.status);
+  //         console.log('Error data:', error.response.data);
+  //         throw error;
+  //       }
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+  // };
+
   // input 값이 변경되면 수정 중인 데이터를 업데이트
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -97,6 +135,7 @@ function GalleryProfileCard() {
 
   const handleComplete = () => {
     setOriginalData(currentData);
+
     handleEditClick();
   };
 
@@ -146,94 +185,101 @@ function GalleryProfileCard() {
     }
     return data;
   };
-  return (
-    <div className="relative flex justify-center items-center mx-auto bg-white shadow-2xl p-1 rounded-lg mx-4 md:mx-auto min-w-1xl max-w-md md:max-w-5xl ">
-      <div className="flex w-full items-start px-4 py-4">
-        <div className="w-1/5 h-50 mr-4 flex flex-col justify-end overflow-hidden">
-          <img className="w-50 h-40 object-cover rounded-xs mb-2 shadow" src={currentData.profileImg} alt="avatar" />
-          {isEditing
-            && (
-              <BaseIconBtnGrey icon="pencil" message="프로필 수정" onClick={handleImgBtnClick} />
-            )}
-          <input
-            type="file"
-            id="imageUpload"
-            accept="image/*"
-            className="hidden"
-            ref={imgInputRef}
-            onChange={handleImageUpload}
-          />
-        </div>
-        <div className="">
-          {isEditing ? (
-            <span className="w-2/5 bg-grey"><input name="name" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" value={currentData.name} type="text" onChange={handleChange} /></span>
-          ) : (
-            <h2 className="flex items-center justify-between text-lg font-semibold text-gray-900 mt-1">{currentData.name}</h2>
-          )}
-          <div className="flex items-center hidden md:block">
-            <div className="flex mr-2 text-gray-700 mr-3">
-              <StarIcon />
-              <span><div className="text-xs text-grey">{currentData.rating}</div></span>
-              <span>
-                <div className="text-xs text-grey">
-                  &#40;
-                  {currentData.reviews}
-                  건&#41;
-                </div>
-              </span>
-              <span>
-                <div className="text-xs text-grey">
-                  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-                  {currentData.like}
-                  명이 좋아한 작가입니다
-                </div>
-              </span>
 
-            </div>
+  /**
+   * 태그 초기화하는 부분
+   */
+  useEffect(() => {
+    tags.forEach((tag) => {
+      if (!tags.includes(tag)) { // 이미 선택된 태그인지 확인
+        dispatch(addSelectedButton(tag));
+      }
+    });
+  }, [tags]);
+
+  return (
+    <div className="bg-white shadow-2xl p-1 rounded-lg mx-4 md:mx-auto min-w-1xl max-w-md md:max-w-5xl mx-auto ">
+      <div className="relative justify-center items-center  ">
+        <div className="flex w-full items-start px-4 py-4">
+          <div className="w-1/5 h-50 mr-4 flex flex-col justify-end overflow-hidden">
+            <img className="w-50 h-40 object-cover rounded-xs mb-2 shadow" src={currentData.profileImg} alt="avatar" />
+            {isEditing
+              && (
+                <BaseIconBtnGrey icon="pencil" message="프로필 수정" onClick={handleImgBtnClick} />
+              )}
+            <input
+              type="file"
+              id="imageUpload"
+              accept="image/*"
+              className="hidden"
+              ref={imgInputRef}
+              onChange={handleImageUpload}
+            />
           </div>
-          <div className="absolute bottom-10 flex items-center mt-15 hidden md:block">
-            <div className="mr-2 text-start text-gray-700 mr-3 ">
-              <div className="text-xs text-black">
-                {initialData.price}
-                원~
+          <div className="">
+            {isEditing ? (
+              <span className="w-2/5 bg-grey"><input name="name" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" value={currentData.name} type="text" onChange={handleChange} /></span>
+            ) : (
+              <h2 className="flex items-center justify-between text-lg font-semibold text-gray-900 mt-1">{currentData.name}</h2>
+            )}
+            <div className="flex items-center hidden md:block">
+              <div className="flex mr-2 text-gray-700 mr-3">
+                <StarIcon />
+                <span><div className="text-xs text-grey">{currentData.rating}</div></span>
+                <span>
+                  <div className="text-xs text-grey">
+                    &#40;
+                    {currentData.reviews}
+                    건&#41;
+                  </div>
+                </span>
+                <span>
+                  <div className="text-xs text-grey">
+                    &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                    {currentData.like}
+                    명이 좋아한 작가입니다
+                  </div>
+                </span>
+
               </div>
-              <div className="flex text-xs text-black">
-                {currentData.tag.map((item) => (
-                  <span key={item} className="mr-2 flex">
-                    <span>
-                      &#35;
-                      {' '}
-                      {item}
+            </div>
+            <div className="absolute bottom-10 flex items-center mt-15 hidden md:block">
+              <div className="text-start text-gray-700 mr-8 ">
+                <div className="flex flex-wrap text-xs text-black mr-40">
+                  {tags.map((item) => (
+                    <span key={item.index} className="mr-2 flex mt-1">
+                      <span><BaseTag message={item.label} /></span>
                     </span>
-                  </span>
-                ))}
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="absolute  top-4 right-4 hidden md:block">
-            <div className="flex w-fit">
-              <span className="mr-1">
-                {isEditing && (
-                  <BaseIconBtnGrey icon="cancel" message="취소하기" onClick={handleCancel} />
-                )}
-              </span>
-              <span className="mr-1">
-                {isEditing ? (
-                  <BaseIconBtnGrey icon="check" message="완료" onClick={handleComplete} />
-                ) : (
-                  <BaseIconBtnGrey onClick={handleEditClick} icon="pencil" message="편집" />
-                )}
-              </span>
+            <div className="flex absolute  top-4 right-4 hidden md:block">
+              <div className="flex w-fit">
+                <span className="mr-1">
+                  {isEditing && (
+                    <BaseIconBtnGrey icon="cancel" message="취소하기" onClick={handleCancel} />
+                  )}
+                </span>
+                <span className="mr-1">
+                  {isEditing ? (
+                    <BaseIconBtnGrey icon="check" message="완료" onClick={handleComplete} />
+                  ) : (
+                    <BaseIconBtnGrey onClick={handleEditClick} icon="pencil" message="편집" />
+                  )}
+                </span>
+              </div>
+              <button type="button" className="flex ml-auto text-xs mt-1 hover:bg-gray-100" onClick={stopArtist}>작가 그만두기</button>
+              <button type="button" className="flex ml-auto text-xs hover:bg-gray-100" onClick={deactivateArtist}>작가 비활성화</button>
             </div>
-            <button type="button" className="flex text-xs mt-1 hover:bg-gray-100" onClick={stopArtist}>작가 그만두기</button>
-            <button type="button" className="flex text-xs hover:bg-gray-100" onClick={deactivateArtist}>작가 비활성화</button>
-          </div>
-          <div className="absolute  bottom-4 right-4">
-            <div className="mb-1"><BaseIconBtnPurple icon="message" message="문의하기" onClick={goChatting} /></div>
-            <div><BaseIconBtnWhite icon="calendar" message="예약하기" onClick={handleReservationBtnClick} /></div>
+            <div className="absolute  bottom-4 right-4">
+              <div className="mb-1"><BaseIconBtnPurple icon="message" message="문의하기" onClick={goChatting} /></div>
+              <div><BaseIconBtnWhite icon="calendar" message="예약하기" onClick={handleReservationBtnClick} /></div>
+            </div>
           </div>
         </div>
       </div>
+      {isEditing && <GalleryTag tags={tags} onTagChange={handleTagChange} />}
     </div>
   );
 }
