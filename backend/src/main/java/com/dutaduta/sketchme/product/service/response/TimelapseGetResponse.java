7d8130d26b4023af412e68697e8337ad595b37ca
허유정 <nikki601@naver.com>
2023-08-18 03:00:43 +0900

    [BE] Fix: 내가 구매한 그림 목록 반환 시 timelapse url도 함께 반환해서 타임랩스 다운로드도 가능하도록 수정

--- backend/src/main/java/com/dutaduta/sketchme/product/service/response/TimelapseGetResponse.java
+++ backend/src/main/java/com/dutaduta/sketchme/product/service/response/TimelapseGetResponse.java
@@ -1,5 +1,6 @@
 package com.dutaduta.sketchme.product.service.response;
 
+import com.dutaduta.sketchme.product.domain.Picture;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,4 +12,10 @@ import lombok.Getter;
 public class TimelapseGetResponse {
     private String timelapseUrl;
     private String thumbnailUrl;
+
+    public static TimelapseGetResponse of(Picture picture){
+        return TimelapseGetResponse.builder()
+                .timelapseUrl(picture.getMeeting().getTimelapse().getUrl())
+                .thumbnailUrl(picture.getMeeting().getTimelapse().getThumbnailUrl()).build();
+    }
 }
