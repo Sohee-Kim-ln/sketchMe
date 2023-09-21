26dd5ebcb5a1ef33f43c91f2d14515e195d05f76
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-21 17:49:43 +0900

    Hotfix: 충돌 제거

--- backend/src/main/java/com/dutaduta/sketchme/videoconference/exception/CheckedException.java
+++
@@ -1,7 +0,0 @@
-package com.dutaduta.sketchme.videoconference.exception;
-
-public class CheckedException extends Exception {
-    public CheckedException(String message){
-        super(message);
-    }
-}
--- backend/src/main/java/com/dutaduta/sketchme/videoconference/exception/NoSessionException.java
+++
@@ -1,8 +0,0 @@
-package com.dutaduta.sketchme.videoconference.exception;
-
-public class NoSessionException extends CheckedException{
-
-    public NoSessionException(String message) {
-        super(message);
-    }
-}
